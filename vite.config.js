be689d856b9a800893dbfecec3ccb23818150a38
Ваше alina <alinaakovenko612@gmail.com>
2024-06-09 17:09:53 +0300

    ddd

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+    }),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
