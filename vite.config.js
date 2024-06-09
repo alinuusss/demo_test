f9051189a02c23816bec28ed948be072fb2679d9
Ваше alina <alinaakovenko612@gmail.com>
2024-06-09 17:19:37 +0300

    ff

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,20 @@ export default defineConfig({
           includeAbsolute: false,
         },
       },
+    }),
+
+    VitePWA({
+      srcDir: "src",
+      filename: "service-worker.js",
+      strategies: "injectManifest",
+      injectRegister: false,
+      manifest: false,
+      injectManifest: {
+        injectionPoint: null,
+      },
+      devOptions: {
+        enabled: true
+      }
     }),
   ],
   base: '/',
